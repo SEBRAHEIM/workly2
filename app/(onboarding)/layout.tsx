402dd0569392f0c22a44b248395a0c56906cd6b0
flightstrips.ATC <pymtydvxbz@privaterelay.appleid.com>
2026-02-26 10:26:05 +0400

    style: remove unnecessary layout wrapper in onboarding

--- app/(onboarding)/layout.tsx
+++ app/(onboarding)/layout.tsx
@@ -4,8 +4,6 @@ export default function OnboardingLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-50">
-            <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-md">{children}</div>
-        </div>
+        <>{children}</>
     )
 }
